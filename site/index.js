636c2d59751345298680d7f2b48549cbeada4431
Amélie <ameliechevalier3006@gmail.com>
2020-04-03 10:15:18 +0200

    add js to display empty glass on click

--- site/index.js
+++ site/index.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
   // TODO : Displaying the empty whisky glass when you click on the glass.
+
+  const whiskyGlass = document.querySelector('.whisky');
+  console.log(whiskyGlass);
+
+  whiskyGlass.addEventListener('click', () => {
+    whiskyGlass.src = 'image/empty_whisky.png';
+  }
+  );
 });
