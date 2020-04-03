eb9425f76affef1aafe798783d46173418b9c62a
Sora952 <sora952c16b10@gmail.com>
2020-04-03 11:37:47 +0200

    Whisky drink !

--- site/index.js
+++ site/index.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', () => {
   // TODO : Displaying the empty whisky glass when you click on the glass.
+  const whiskyElt = document.querySelector('.whisky');
+
+  whiskyElt.addEventListener('click', (e) => {
+    whiskyElt.setAttribute('src', 'image/empty_whisky.png')
+  });
 });
