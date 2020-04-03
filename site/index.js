2d4e1160a90285fe928c54e6e04e6f11e0e0731f
Astarosa <baptiste.shortgibet@gmail.com>
2020-04-03 13:56:40 +0200

    js work

--- site/index.js
+++ site/index.js
@@ -1,9 +1,10 @@
-function toggleImage() {
-  const img1 = "image/whisky.png";
-  const img2 = "image/empty_whisky.png";
-  
-  let imgElement = document.getElementsByClassName('whisky');
-
-  imgElement.src = (imgElement.src === img1)? img2 : img1;
-}
-/* marche pas jsp pas pourquoi */
+window.addEventListener('load', () => {
+    const img2 = "image/empty_whisky.png";
+    
+    let imgElement = document.getElementById('whisky-switch');
+    imgElement.addEventListener('click', function() {
+      console.log(imgElement.src);
+      imgElement.src = img2
+      
+    });
+});
