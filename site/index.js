4215a2e66492f9c77e76a39d5aa71842c69e96e6
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-03 12:33:54 +0200

    DOM creation went to be a disaster

--- site/index.js
+++ site/index.js
@@ -1,3 +1,13 @@
-window.addEventListener('load', () => {
-  // TODO : Displaying the empty whisky glass when you click on the glass.
+const firstImg = document.getElementById('whisky');
+let count = 0;
+
+firstImg.addEventListener('click', () => {
+  count += 1;
+  const secondImg = <img src="image/empty_whisky.png" alt="an empty whisky glass" class="emptyWhisky" id="emptyWhisky" />;
+  firstBtn.innerHTML = img;
 });
+
+//window.addEventListener('load', () => {
+  // TODO : Displaying the empty whisky glass when you click on the glass.
+//});
+
