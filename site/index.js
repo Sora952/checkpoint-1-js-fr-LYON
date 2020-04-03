1bb6af3b7e41482e669847e31fb4c2335e255eaa
Astarosa <baptiste.shortgibet@gmail.com>
2020-04-03 13:17:37 +0200

    js for whisky

--- site/index.js
+++ site/index.js
@@ -1,4 +1,9 @@
-window.addEventListener('load', () => {
-  document.getElementsByClassName('.whisky')
-  /*pas eu le temps de finir */ 
-});
+function toggleImage() {
+  const img1 = "image/whisky.png";
+  const img2 = "image/empty_whisky.png";
+  
+  let imgElement = document.getElementsByClassName('whisky');
+
+  imgElement.src = (imgElement.src === img1)? img2 : img1;
+}
+/* marche pas jsp pas pourquoi */
