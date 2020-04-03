bf70966d97be11dea8941850ab540b9fc608797c
Geoffroy <geoffroy.crabieres@gmail.com>
2020-04-03 10:17:58 +0200

    finished HTML & CSS exercise

--- site/index.js
+++ site/index.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', () => {
   // TODO : Displaying the empty whisky glass when you click on the glass.
 });
+
+function emptyGlass() {
+  const whiskyGlass = document.getElementById("whiskyGlass");
+  whiskyGlass.src='image/empty_whisky.png';
+}
