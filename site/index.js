b034583dcc314517422d4b20d08fe94814fdd31c
Rincevent42 <porvooc@gmail.com>
2020-04-03 11:53:20 +0200

    feat: secret book steps 5 and 6

--- site/index.js
+++ site/index.js
@@ -1,3 +1,10 @@
-window.addEventListener('load', () => {
-  // TODO : Displaying the empty whisky glass when you click on the glass.
+window.addEventListener('click', () => {
+  // TODO : Displaying the empty whisky glass when you click on the glass. 
+  if (document.getElementsByClassName('whisky').src === './image/whisky.png') {
+    document.getElementsByClassName('whisky').src = './image/empty_whisky.png';
+    console.log('empty_whisky');
+  } else {
+    document.getElementsByClassName('whisky').src = './image/whisky.png';
+    console.log('whisky');
+  }
 });
