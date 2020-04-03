96f6ff23f989ee9a037322e6f286e7521fd5f6ae
Kosi <hugo.pioda@gmail.com>
2020-04-03 13:05:26 +0200

    Empty the glass JS

--- site/index.js
+++ site/index.js
@@ -1,10 +1,7 @@
-const img = document.getElementById('whisky');
+window.addEventListener('load', () => {
+  const img = document.getElementById('whisky');
 
-function changeImg () {
-  if (img.src !== './image/empty_whisky.png') {
-    img.src = './image/empty_whisky.png';
-  } else {
-    img.src = './image/whisky.png';
-  }
-}
-img.addEventListener ('click', changeImg(), true);
+  img.addEventListener('click', () => {
+    img.src = 'image/empty_whisky.png';
+  });
+});
