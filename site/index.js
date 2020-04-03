986844f81585b9c4f72e0ea13657f1eec684e628
Vianney Bouault <vbouault@hotmail.fr>
2020-04-03 12:08:17 +0200

    exo html/css

--- site/index.js
+++ site/index.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', () => {
   // TODO : Displaying the empty whisky glass when you click on the glass.
+  const imgWhisky = document.getElementById('whisky');
+  imgWhisky.addEventListener('click',() =>{
+    imgWhisky.src = 'image/empty_whisky.png'
+  })
 });
+
