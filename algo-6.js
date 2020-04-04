97814889ff3726e3fcf86d7ca3f47ae0cca0a0aa
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-04-04 11:05:57 +0200

    Algo 6 solved git add .

--- algo-6.js
+++ algo-6.js
@@ -12,6 +12,37 @@ sumArr( ["2", "7", "3", "8", "2"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "
 
 */
 
+let arrayA = ["1", "2", "3"];
+let arrayB = ["2", "4", "1"];
+
 function sumArr(arrayA, arrayB) {
   // Your code here !
-}
+
+  //Calcul du tableau le plus long afin d'itérer sur celui-ci et de n'oublier aucune valeur
+  let longestArray = "";
+  if (arrayA.length >= arrayB.length) {
+    longestArray = arrayA;
+  } else {
+    longestArray = arrayB;
+  }
+
+  // Création d'un tableau afin de pusher l'addition des number de chacun des 2 tableaux inputs
+  outputArray = [];
+  for (let i = 0; i < longestArray.length; i++) {
+    if (typeof arrayA[i] === "undefined") {
+      outputArray.push(Number(arrayB[i]));
+    } else if (typeof arrayB[i] === "undefined") {
+      outputArray.push(Number(arrayA[i]));
+    } else {
+      outputArray.push(Number(arrayA[i]) + Number(arrayB[i]));
+    }
+  };
+
+  // Conversion du tableau en string
+  for (let i = 0; i < outputArray.length; i++) {
+    outputArray[i] = outputArray[i].toString();
+  };
+  return outputArray;
+};
+
+console.log(sumArr(arrayA, arrayB));
