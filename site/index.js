5b01a7c01a0372e3566321a318adda12c7207def
tmicka23 <mickathp42@gmail.com>
2020-04-04 18:08:25 +0200

    HANDLE whisky glass with JS

--- site/index.js
+++ site/index.js
@@ -1,3 +1,15 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   // TODO : Displaying the empty whisky glass when you click on the glass.
+  const handleWhisky = () => {
+    const glass = document.querySelector("#glass");
+    console.log(glass);
+    glass.addEventListener("click", () => {
+      if (glass.getAttribute("src") === "/site/image/whisky.png") {
+        glass.setAttribute("src", "/site/image/empty_whisky.png");
+      } else {
+        glass.setAttribute("src", "/site/image/whisky.png");
+      }
+    });
+  };
+  handleWhisky();
 });
