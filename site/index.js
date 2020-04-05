6fef1ac475687906a7c085fea9ae93572930850d
riri6969 <riri.zamor@gmail.com>
2020-04-06 00:40:06 +0200

    html and css done

--- site/index.js
+++ site/index.js
@@ -1,10 +1,3 @@
 window.addEventListener('load', () => {
-  function toggleImage() {
-    var img1 = "image/whisky.png";
-    var img2 = "image/empty_whisky.png";
-    
-    var imgElement = document.getElementById('toggleImage');
  
-    imgElement.src = (imgElement.src === img1)? img2 : img1;
- }
 });
