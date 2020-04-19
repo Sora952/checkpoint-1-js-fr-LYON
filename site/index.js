48d265b5511d391699eb3008314ce319815594f1
Vianney Bouault <vbouault@hotmail.fr>
2020-04-19 11:39:35 +0200

    ajout table

--- site/index.js
+++ site/index.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', () => {
   // TODO : Displaying the empty whisky glass when you click on the glass.
   const imgWhisky = document.getElementById('whisky');
+ 
   imgWhisky.addEventListener('click',() =>{
     imgWhisky.src = 'image/empty_whisky.png'
   })
+
 });
 
